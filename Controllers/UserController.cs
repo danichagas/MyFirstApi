b252dddb0b4734a8afbf7a6665fab84d9df43654
danichagas <chagasdaniel788@gmail.com>
2025-04-15 16:14:09 -0300

    feat: my first endpoint

---
+++ Controllers/UserController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyFirstApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UserController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult UserName()
+    {
+        return Ok("Tralalelo tralala");
+    }
+}
