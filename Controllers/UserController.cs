9692fc30da79412e76705df1b4b8c9cbe7dd516d
danichagas <chagasdaniel788@gmail.com>
2025-04-16 13:34:53 -0300

    feat: creating a post type endpoint

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyFirstApi.Communication.Requests;
+using MyFirstApi.Communication.Responses;
 
 namespace MyFirstApi.Controllers;
 
@@ -6,10 +8,22 @@ namespace MyFirstApi.Controllers;
 [ApiController]
 public class UserController : ControllerBase
 {
+    [HttpPost]
+    [ProducesResponseType(typeof(ResponseRegisterUserJson), StatusCodes.Status201Created)]
+    public IActionResult CreateUser([FromBody] RequestRegisterUserJson request)
+    {
+        var response = new ResponseRegisterUserJson
+        {
+            Id = 1,
+            Name = request.Name
+        };
+        return Created(string.Empty, response);
+    }
+
     [HttpGet]
     [Route("{id}/email/{email}")]
     [ProducesResponseType(StatusCodes.Status200OK)]
-    public IActionResult UserName(int id, string email)
+    public IActionResult GetUser(int id, string email)
     {
         var response = new User
         {
